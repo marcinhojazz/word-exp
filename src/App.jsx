dad3a202d7aec89c3e65288585e6806f2f24e07c
marcinhojazz <marcinhojazz@gmail.com>
2023-07-28 23:45:43 -0300

    update cene camera

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stats } from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
+import { useEffect } from "react";
+import { OrbitControls, OrthographicCamera, PerspectiveCamera, Stats } from "@react-three/drei";
 import AnimatedBox from "./components/AnimatedBox";
-import CameraOrbitController from "./components/CameraOrbitController";
 import Player from "./components/Player";
 
 function App() {
   const testing = true;
   
   return (
-    <Canvas shadows>
+    <Canvas shadows or>
       {testing ? <Stats /> : null}
       {testing ? <gridHelper args={[20, 20]} /> : null}
       {testing ? <axesHelper visible={'testing'} args={[3]}/> : null}
       <Player />
-      <OrbitControls />
+      <OrbitControls position={[0,3,5]}/>
       <ambientLight intensity={0.5} />
       <directionalLight color="yellow" position={[0,0,5]} />
+      <PerspectiveCamera position={[1.5, 3.2, 3.5]} makeDefault />
+
       <AnimatedBox isTesting={testing} />
+      <CameraLogger />
     </Canvas>
   );
+}
+
+function CameraLogger() {
+  const { camera } = useThree();
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      console.log(camera.position);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [camera]);
+
+  return null;
 }
 
 export default App;
