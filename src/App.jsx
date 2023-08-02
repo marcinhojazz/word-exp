9456ce44897afbb44eca58eb8772ece8c100a735
marcinhojazz <marcinhojazz@gmail.com>
2023-08-02 09:47:29 -0300

    add help controls img

--- src/App.jsx
+++ src/App.jsx
@@ -9,38 +9,43 @@ function App() {
   const testing = true;
   
   return (
-    <Canvas shadows>
-      {testing ? <Stats /> : null}
-      {testing ? <gridHelper args={[20, 20]} /> : null}
-      {testing ? <axesHelper visible={'testing'} args={[3]}/> : null}
-      <Player />
-      <OrbitControls position={[0,3,5]}/>
-      <ambientLight intensity={0.6} />
-      <directionalLight 
-        color="white" 
-        position={[0,2,5]} 
-        intensity={0.5} 
-        castShadow
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={50}
-        shadow-camera-left={-10}
-        shadow-camera-right={10}
-        shadow-camera-top={10}
-        shadow-camera-bottom={-10}
-      />
-      <spotLight 
-        position={[10, 10, 10]} 
-        angle={0.3} 
-        intensity={0.5} 
-        castShadow
-      />
-      <PerspectiveCamera position={[1.5, 3.2, 3.5]} makeDefault />
-      <Ground />
+    <>
+      <Canvas shadows>
+        {testing ? <Stats /> : null}
+        {testing ? <gridHelper args={[20, 20]} /> : null}
+        {testing ? <axesHelper visible={'testing'} args={[3]}/> : null}
+        <Player />
+        <OrbitControls position={[0,3,5]}/>
+        <ambientLight intensity={0.6} />
+        <directionalLight 
+          color="white" 
+          position={[0,2,5]} 
+          intensity={0.5} 
+          castShadow
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <spotLight 
+          position={[10, 10, 10]} 
+          angle={0.3} 
+          intensity={0.5} 
+          castShadow
+        />
+        <PerspectiveCamera position={[1.5, 3.2, 3.5]} makeDefault />
+        <Ground />
 
-      <AnimatedBox isTesting={testing} />
-      <CameraLogger />
-    </Canvas>
+        <AnimatedBox isTesting={testing} />
+        <CameraLogger />
+      </Canvas>
+      <div className="helpcontrols">
+        <img width={400} src="/helpcontrol.png" alt="help" />
+      </div>
+    </>
   );
 }
 
